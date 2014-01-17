4ceea1405e9da4db525821d94650810b6a8a5882
Milotskiy Alexey <spalaxinco@gmail.com>
2014-01-17 15:42:06 +0200

    Added custom events to fire them when at least one element selected in grid, and when all elements are deselected.

--- public/js/src/hc-backend/dgrid/_Selection.js
+++ public/js/src/hc-backend/dgrid/_Selection.js
@@ -1,12 +1,15 @@
 define([
     "dojo/_base/declare",
+    "dojo/_base/lang",
     "dgrid/_StoreMixin",
+    "dijit/Destroyable",
+    "dojo/on",
     "dgrid/Selection"
-], function(declare, _StoreMixin, Selection) {
+], function(declare, lang, _StoreMixin, Destroyable, on, Selection) {
     // module:
     //     backend/dgrid/_SelectionWithRemoveFromStore
 
-    return declare([ _StoreMixin, Selection ], {
+    return declare([ _StoreMixin, Selection, Destroyable ], {
         //  summary:
         //      Expanded selection for removeSelected rows from store
         removeSelected: function () {
@@ -22,6 +25,32 @@ define([
             } catch (e) {
                 console.error(this.declaredClass, arguments, e);
                 throw e;
+            }
+        },
+
+        postCreate: function () {
+            try {
+                this.inherited(arguments);
+
+                var _func = lang.hitch(this, function (){
+                    on.emit(this.domNode, 'dgrid-one-item-selected',{bubbles: true,
+                                                                     cancelable: false,
+                                                                     grid: this});
+                });
+                this.own(on.once(this, 'dgrid-select', _func));
+
+                this.own(on(this, 'dgrid-deselect', lang.hitch(this, function (evt){
+                    for (var id in evt.grid.selection) return;
+                    on.emit(this.domNode,
+                            'dgrid-no-items-selected',
+                            {bubbles: true,
+                             cancelable: false,
+                             grid: this});
+                    this.own(on.once(this, 'dgrid-select', _func));
+                })));
+            } catch (e) {
+                 console.error(this.declaredClass, arguments, e);
+                 throw e;
             }
         },
 
