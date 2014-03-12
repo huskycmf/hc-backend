048a1abc7795d34a86df40ac19c687d86baaec82
Oleksii Mylotskyi <oleksii.mylotskyi@milsdev.com>
2014-03-12 20:38:50 +0200

    Modernize build_dependencies, to build full stack dependencies from every included plugin

--- public/js/src/hc-backend/build_dependencies.sh
+++ public/js/src/hc-backend/build_dependencies.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
-
 full="";
-for i in `find . -name "*.js" -print | grep -v './backend.profile.js'`;
-  do 
-    i=${i:2};
-    i=${i%.*};
-    full=$full"\"backend/"$i"\", "
+echo "define([" > Deps.js;
+
+while [ "$1" != "" ]; do
+    echo "DIR >>${1}";
+    cd ${1};
+    for i in `find . -name '*.js' -print | grep -v './*.profile.js' | grep -v './tests'`;
+    do 
+        echo "FILE >>"$i
+        i=${i:2};
+        i=${i%.*};
+        
+        if [ "${1}" == "." ]; then
+            module=${PWD##*/};
+        else
+            module=${1};
+        fi
+
+        full=$full"\"$module/"$i"\", "
+    done;
+    cd -;
+    echo "${full%,*}," >> Deps.js;
+    shift;
 done;
-echo "define(["${full%,*}"], function(){});" > Deps.js;
+
+echo "], function(){});" >> Deps.js;
