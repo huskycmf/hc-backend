7ce574c6d3f921b092dd8bad57d44503765c178b
Oleksii Mylotskyi <spalaxinco@gmail.com>
2014-03-23 13:33:48 +0200

    Changed dependency from Page instance to the PageInterface.

--- src/HcBackend/Stdlib/Extractor/Page/Extractor.php
+++ src/HcBackend/Stdlib/Extractor/Page/Extractor.php
@@ -1,24 +1,24 @@
 <?php
 namespace HcBackend\Stdlib\Extractor\Page;
 
+use HcBackend\Entity\PageInterface;
 use Zf2Libs\Stdlib\Extractor\ExtractorInterface;
 use Zf2Libs\Stdlib\Extractor\Exception\InvalidArgumentException;
-use HcBackend\Entity\Page as PageEntity;
 
 class Extractor implements ExtractorInterface
 {
     /**
      * Extract values from an object
      *
-     * @param PageEntity $pageEntity
+     * @param PageInterface $pageEntity
      * @param string $prefix
      * @throws \Zf2Libs\Stdlib\Extractor\Exception\InvalidArgumentException
      * @return array
      */
     public function extract($pageEntity)
     {
-        if (!$pageEntity instanceof PageEntity) {
-            throw new InvalidArgumentException("Expected HcBackend\\Entity\\Page object, invalid object given");
+        if (!$pageEntity instanceof PageInterface) {
+            throw new InvalidArgumentException("Expected HcBackend\\Entity\\PageInterface object, invalid object given");
         }
 
         return array('pageTitle'    => $pageEntity->getTitle(),
