f6aee0e4c8e3affa111f1d7754eae2aacddde1c3
Oleksii Mylotskyi <spalaxinco@gmail.com>
2015-01-08 02:38:18 +0200

    Implemented support for default Image Create and Image Delete.

--- config/module/di/instance/alias.config.php
+++ config/module/di/instance/alias.config.php
@@ -1,7 +1,17 @@
 <?php
 return array(
     'ZfcUser-LoginForm' => 'ZfcUser\Form\Login',
-    'HcBackend-Images-Default-TotalImagesRemover' => 'Zf2FileUploader\Resource\Handler\Remover\AggregateRemover',
-    'HcBackend-Images-Default-CleanerStrategy' => 'Zf2FileUploader\Service\Cleaner\ImageTemporaryCleaner',
-    'HcBackend-Images-Default-SaveService' => 'Zf2FileUploader\Service\Image\SaveService'
+    'HcBackend-Images-TotalImagesRemover' => 'Zf2FileUploader\Resource\Handler\Remover\AggregateRemover',
+    'HcBackend-Images-CleanerStrategy' => 'Zf2FileUploader\Service\Cleaner\ImageTemporaryCleaner',
+    'HcBackend-Service-Image-SaveService' => 'Zf2FileUploader\Service\Image\SaveService',
+    'HcBackend-View-Model-UploaderModel' => 'Zf2FileUploader\View\Model\UploaderModel',
+    'HcBackend-InputFilter-Image-CreateResource' => 'Zf2FileUploader\InputFilter\Image\CreateResource',
+    'HcBackend-Input-Image-CreateResource' => 'Zf2FileUploader\Input\Image\CreateResource',
+
+    // Controllers
+    'HcBackend-Controller-Images-Create' => 'Zf2FileUploader\Controller\Images\CreateController',
+
+    //ViewModels
+    'HcBackend-View-Model-User-Login' => 'HcBackend\View\Model\User\LoginModel'
+
 );
