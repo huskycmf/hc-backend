9b114de30d905d13d6aada45f1012ff986b61886
Oleksii Mylotskyi <spalaxinco@gmail.com>
2015-03-19 20:58:02 +0200

    Fixed bug, when switching between tabs, onHide event does not triggered.

--- public/js/src/hc-backend/layout/ContentPane.js
+++ public/js/src/hc-backend/layout/ContentPane.js
@@ -1,11 +1,23 @@
 define([
-    "dijit/layout/ContentPane"
-], function(ContentPane) {
+    "dijit/layout/ContentPane",
+    "dojo/_base/array"
+], function(ContentPane, array) {
     // summary:
     //      Base ContentPane with doLayout = false by default
     //      we do not need to ContentPane to doLayouts
 
     return require('dojo/_base/declare')([ ContentPane ], {
-            doLayout: false
+            doLayout: false,
+
+            onHide: function () {
+                try {
+                    array.map(this.getChildren(), function (child) {
+                        child.onHide && child.onHide();
+                    });
+                } catch (e) {
+                     console.error(this.declaredClass, arguments, e);
+                     throw e;
+                }
+            }
     });
 });
--- public/js/src/hc-backend/layout/StackContainer.js
+++ public/js/src/hc-backend/layout/StackContainer.js
@@ -1,11 +1,23 @@
 define([
-    "dijit/layout/StackContainer"
-], function(StackContainer) {
+    "dijit/layout/StackContainer",
+    "dojo/_base/array"
+], function(StackContainer, array) {
     // summary:
     //      Base StackContainer with doLayout = false by default
     //      we do not need to StackContainer to doLayouts
 
     return require('dojo/_base/declare')([ StackContainer ], {
-            doLayout: false
+            doLayout: false,
+
+            onHide: function () {
+                try {
+                    array.map(this.getChildren(), function (child) {
+                        child.onHide && child.onHide();
+                    });
+                } catch (e) {
+                    console.error(this.declaredClass, arguments, e);
+                    throw e;
+                }
+            }
     });
 });
